bfc42359c133f0cc3cf8ec4013f21161f775de86
Maksym <mzimak18@gmail.com>
2021-02-20 14:06:42 +0100

    Modify Program.cs

---
+++ hada-p1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hada_p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What unit does it start with? s - seconds, m - minutes");
+            string choose = Console.ReadLine();
+            Console.WriteLine("How many?");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            if (choose == "s")
+            {
+                Console.WriteLine(HadaP1.Seconds2Minutes(amount));
+            }
+            if (choose == "m")
+            {
+                Console.WriteLine(HadaP1.Minutes2Seconds(amount));
+            }
+            Console.ReadLine();
+        }
+    }
+}
