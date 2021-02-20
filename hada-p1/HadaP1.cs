9ea642030e9d25099177703841b2db41bfc25e59
Maksym <mzimak18@gmail.com>
2021-02-20 13:32:06 +0100

    Modify HadaP1.cs

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -11,6 +11,11 @@ namespace hada_p1
         public static double Seconds2Minutes(double s) {
             return s==0 ? 0 : s/60; 
         }
+
+        public static double Minutes2Seconds(double m)
+        {
+            return m == 0 ? 0 : m * 60;
+        }
         static void Main(string[] args)
         {
           
