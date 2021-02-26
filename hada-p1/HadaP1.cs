f48c004b41092dd1954e9895c776a5bd326d6b34
Maksym <mzimak18@gmail.com>
2021-02-26 14:02:31 +0100

    Modify of HadaP1

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿///using System;
+///using System.Collections.Generic;
+///using System.Linq;
+///using System.Text;
+///using System.Threading.Tasks;
 
-namespace hada_p1
-{
-    class HadaP1
-    {
-        public static double Seconds2Minutes(double s) {
-            return s==0 ? 0 : s/60; 
-        }
+///namespace hada_p1
+///{
+///    class HadaP1
+///    {
+///       public static double Seconds2Minutes(double s) {
+///            return s==0 ? 0 : s/60; 
+///}
 
-        public static double Minutes2Seconds(double m)
-        {
-            return m == 0 ? 0 : m * 60;
-        }
-        static void Main(string[] args)
-        {
-          
-        }
-    }
-}
+///public static double Minutes2Seconds(double m)
+///    {
+///            return m == 0 ? 0 : m * 60;
+///}
+///    static void Main(string[] args)
+///    {
+
+///}
+///}
+///}
